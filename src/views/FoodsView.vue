1964fba3f592eee75d93c4939687e1d376e2420a
unknown <enny.9h08@gmail.com>
2023-07-31 21:26:13 +0700

    first commit

---
+++ src/views/FoodsView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <NavbarSite />
+    <h2>Daftar Makanan</h2>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavbarSite from "@/components/NavbarSite.vue";
+
+export default {
+  name: "FoodsView",
+  components: {
+    NavbarSite,
+  },
+};
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <NavbarSite />
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavbarSite from "@/components/NavbarSite.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    NavbarSite,
+  },
+};
+</script>
